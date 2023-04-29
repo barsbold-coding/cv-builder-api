29f7a430dedca4d27465b5153c44d31f5257345e
jackere1 <jackerenh@gmail.com>
2023-04-29 21:40:12 +0800

    added gpt

---
+++ src/modules/gptModule/gpt.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { Configuration, OpenAIApi } from 'openai'
+import * as dotenv from 'dotenv'
+
+@Injectable()
+export class GptService {
+    configuration: Configuration;
+    openai: OpenAIApi;
+
+    constructor() {
+        dotenv.config();
+        this.configuration = new Configuration({
+            apiKey: process.env.GPT_KEY as string
+        });
+        this.openai = new OpenAIApi(this.configuration);
+    }
+
+    getMbtiInfo(mbti): string {
+        const result: any = this.openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                role: 'user',
+                content: mbti
+            }]
+        }).catch(err => {
+            //TO-DO error handler
+        });
+
+        return result.data.choices[0].message;
+    }
+
+}
