e410ffdd046163e618ac1e46e916011c222e147a
Dakiee <ldavaanyam@gmail.com>
2023-04-29 21:08:14 +0800

    aaa

--- src/modules/userModule/user.controller.ts
+++ src/modules/userModule/user.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Firestore } from 'firebase/firestore';
 
 @Controller('users')
 export class UserController {
-  @Get(':email')
-  findOneByUser() {
-    
+  private firestore: Firestore;
+
+  @Get('/:email')
+  async getUser(@Param('email') email: string) {
+    try {
+      const docSnapshot = await this.firestore
+        .collection('Detail')
+        .doc(email)
+        .get();
+      if (docSnapshot.exists) {
+        const userData = docSnapshot.data();
+        return userData;
+      } else {
+        throw { error: 'User not found' };
+      }
+    } catch (error) {
+      console.error('Error : ', error);
+      throw { error: 'Failed to retrieve user information' };
+    }
+  }
+
+  @Post()
+  async createUser(@Body() userData: any) {
+    const customId = userData.id;
+    try {
+      await this.firestore.collection('Detail').doc(customId).set(userData);
+      console.log('id: ', customId);
+      return { msg: 'User information added' };
+    } catch (error) {
+      console.error('Error adding document: ', error);
+      throw { error: 'Failed to add user information' };
+    }
   }
 }
